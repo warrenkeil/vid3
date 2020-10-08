ffcdcbbf5f56b1fee0534f99d223e23a6ec3c437
Warren Keil <wdkeil@gmail.com>
2020-10-08 18:14:35 -0500

    Finished Exercise 1

--- vid3/App_Start/BundleConfig.cs
+++ vid3/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace vid3
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap-lumen.css",
+                      "~/Content/bootstrap - Copy.css",
                       "~/Content/site.css"));
         }
     }
